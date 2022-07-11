563b14a4f1d9f10702e489c914f820d7c089a202
student <orishohat@cyber4s.devorishohatgit config --global user.email orishohat@cyber4s.dev>
2022-07-11 14:23:30 +0300

    srtyling pokemons

--- src/public/js/components/Pokemon.ts
+++ src/public/js/components/Pokemon.ts
@@ -1,5 +1,6 @@
 import { IPokemon } from "../Data";
 
+
 // Renders a given pokemon to its parent.
 class Pokemon {
 	static render(pokemon: IPokemon, parent: HTMLElement) {
@@ -12,7 +13,7 @@ class Pokemon {
 			<img src="${pokemon.img || "./img/default.png"}" alt="${pokemon.name} pokemon">
 		</div>
 		<div class="info">
-			<p class="type"><span>specie: </span>${pokemon.specie}</p>
+			<p class="type"><span>specie: </span><span class="${pokemon.specie}">${pokemon.specie}</span></p>
 			<p class="height"><span>height: </span>${pokemon.height}</p>
 			<p class="weight"><span>weight: </span>${pokemon.weight}</p>
 		</div>
